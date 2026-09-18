973ebcd220b8ca240611479c2fab6a47fa012f65
reigadegr <2722688642@qq.com>
2026-09-18 11:02:33 +0800

    feat: import logger

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use chrono::Local;
+use std::{fmt, io::IsTerminal};
+use tracing_subscriber::{
+    EnvFilter,
+    fmt::{format::Writer, time::FormatTime},
+};
+
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+struct LoggerFormatter;
+
+impl FormatTime for LoggerFormatter {
+    fn format_time(&self, w: &mut Writer<'_>) -> fmt::Result {
+        write!(w, "{}", Local::now().format("%Y-%m-%d %H:%M:%S"))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let is_terminal = std::io::stdout().is_terminal();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(env_filter)
+        .with_timer(LoggerFormatter)
+        .with_ansi(is_terminal)
+        .init();
 }
